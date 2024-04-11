b1265232c591ee2990521b5b57e60339163ac2ab
Donevyn X. Bellido <dbellido@bellarmine.edu>
2024-04-11 15:13:52 -0400

    second commit

---
+++ Javaproject.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Donevyn Bellido
+ */
+public class Javaproject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dr.Johnson! This is Donevyn Bellido");
+
+	}
+
+}
